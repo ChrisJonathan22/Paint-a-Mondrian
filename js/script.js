aef62c9193aa2d5cef49eed9c13b180b2d8a0b90
ChrisJonathan22 <christophereko@hotmail.fr>
2018-03-11 22:05:57 +0000

    Paint a Mondrian
    Second Commit.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,6 @@
 // This was the most challenging project for me so far. I looked for answers everywhere but I couldn't find anything that worked for me so I kept going. I decided to inspect the page and look at how she(Jennifer Dewalt) made it happen with javascript only to find out that she used jQuery which I'm avoiding in order to learn Vanilla JS. So I decided to convert the jQuery to Vanilla JS which wasn't easy but it felt so good when I made it work.
 
 
-var red = document.getElementById("red");
-var blue = document.getElementById("teal");
-var green = document.getElementById("green");
-var pink = document.getElementById("pink");
-
-
-
 var paint = "white";
 
 var color = document.getElementsByClassName("color");
@@ -26,7 +19,7 @@ for(var i = 0; i < color.length; i++){
 }
 
 for(var i = 0; i < box.length; i++) {
-    box[i].addEventListener("click",  function setColor(){
+    box[i].addEventListener("click",  function(){
         this.style.backgroundColor = paint;
     }, false); 
 }
